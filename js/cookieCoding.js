37e4f5f0119cab0443c44ed36696b2a5f91e889b
xunhanliu <1638081534@qq.com>
2018-07-14 22:01:23 +0800

    Site updated: 2018-07-14 22:01:15

--- js/cookieCoding.js
+++ js/cookieCoding.js
@@ -2,13 +2,13 @@ function getCookie(c_name)
 {
     if (document.cookie.length>0)
     {
-        c_start=document.cookie.indexOf(c_name + "=")
+        c_start=document.cookie.indexOf(c_name + "=");
         if (c_start!=-1)
         {
-            c_start=c_start + c_name.length+1
-            c_end=document.cookie.indexOf(";",c_start)
-            if (c_end==-1) c_end=document.cookie.length
-            return unescape(document.cookie.substring(c_start,c_end))
+            c_start=c_start + c_name.length+1;
+            c_end=document.cookie.indexOf(";",c_start);
+            if (c_end==-1) c_end=document.cookie.length;
+            return unescape(document.cookie.substring(c_start,c_end));
         }
     }
     return ""
@@ -16,10 +16,10 @@ function getCookie(c_name)
 
 function setCookie(c_name,value,expiredays)  //һ�Ӿ���һ��
 {
-    var exdate=new Date()
-    exdate.setDate(exdate.getDate()+expiredays)
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate()+expiredays);
     document.cookie=c_name+ "=" +escape(value)+
-        ((expiredays==null) ? "" : "; expires="+exdate.toGMTString())
+        ((expiredays==null) ? "" : "; expires="+exdate.toGMTString());
 }
 
 function checkCookie()
@@ -34,9 +34,9 @@ function checkCookie()
         while(1) {
             username = prompt('Please enter your name:', "");
             if (username != null && username != "") {
-                var buf=CodingJS.myCoding(username).toString()
+                var buf=CodingJS.myCoding(username).toString();
                 if( CodingJS.myCoding(buf).toString()=="68f727115522736adc310087036bcaf201922b6b"){
-                    setCookie('username', buf, 1)
+                    setCookie('username', buf, 1);
                     break;
                 }
             }
